8767bce14f5b54487311b95d23fa9a79539034f6
xxarupakaxx <arupaka3tdigks@gmail.co>
2025-11-25 20:59:48 +0900

    delete cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://depthforge.pages.dev', // Cloudflare Pages のデフォルトドメイン（後で変更可能）
+  site: 'https://depth-forge.netlify.app', // Netlify のドメイン
   vite: {
     plugins: [tailwindcss()]
   }
